71ca7f95c4e374c581e06e8e7b1ba1d55a0b9641
ahmed-umair <43809813+ahmed-umair@users.noreply.github.com>
2018-11-17 16:23:45 +0300

    model files
    first model src files uploaded

---
+++ src/model/Main.java
@@ -0,0 +1,10 @@
+package com.iqpuzzler.model;
+
+public class Main {
+
+    public static void main(String[] args) {
+        XYCoord coordinate = new XYCoord(5,6);
+        System.out.println(coordinate.getX());
+        System.out.println(coordinate.getY());
+    }
+}
---
+++ src/model/XYCoord.java
@@ -0,0 +1,19 @@
+package com.iqpuzzler.model;
+
+public class XYCoord {
+    private int x;
+    private int y;
+
+    XYCoord(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
